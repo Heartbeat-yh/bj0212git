0a1d317def0105c7e585402865569434f5c3726e
yh <13333967390@163.com>
2022-05-27 16:16:24 +0800

    2.hotfix提交代码

--- src/main/java/com/yh/GitTest.java
+++ src/main/java/com/yh/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("master 写的代码");
+        System.out.println("hhotfix ");
     }
 }
