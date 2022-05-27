a1c0ce24d51cdac61dda8dedc3a1fa3a74298815
yh <13333967390@163.com>
2022-05-27 16:10:22 +0800

    1.idea测试git提交

---
+++ src/main/java/com/yh/GitTest.java
@@ -0,0 +1,12 @@
+package com.yh;
+
+/**
+ * @author yh
+ * @version 1.0
+ * @date 2022/5/27 15:53
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
